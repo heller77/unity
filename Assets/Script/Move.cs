fe257ffc2d99770641825a8c18d41c07a701b1f5
koki <koki001230@icloud.com>
2020-02-26 12:28:51 +0900

    create

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Vector3 position;
+    private int speed=5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position=transform.position;
+        
+      if(Input.GetKey(KeyCode.W)){
+        position.x +=speed*Time.deltaTime;
+        transform.position=position;
+      }
+      if(Input.GetKey(KeyCode.S)){
+        position.x-=speed*Time.deltaTime;
+        transform.position=position;
+      }
+       if(Input.GetKey(KeyCode.A)){
+        position.z +=speed*Time.deltaTime;
+        transform.position=position;
+      }
+      if(Input.GetKey(KeyCode.D)){
+        position.z-=speed*Time.deltaTime;
+        transform.position=position;
+      }
+      
+      
+    }
+}
